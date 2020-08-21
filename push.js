b73e745f4ba629df06a6f4b68236c02185000748
Particle45 <sidehustle003@gmail.com>
2020-08-21 16:16:13 +0530

    fix

--- push.js
+++ push.js
@@ -5,7 +5,7 @@ let vapidkeys = {
     privateKey: 'oW21wHiYb3Ek4abDBCHma5xJWV_prq_W5oVt0pHgHio'
   } 
 
-push.setVapidDetails('mailto:sds@1.co.uk',vapidkeys.publicKey, vapidkeys.privateKey)
+push.setVapidDetails('mailto:rishabhbagul3@gmail.com',vapidkeys.publicKey, vapidkeys.privateKey)
 
 
 let sub = {endpoint:"https://fcm.googleapis.com/fcm/send/fLqJKTzr3AU:APA91bFG--8TmzbOydYmLNh1P3Sbpjc7QPbQgGDOtrGcEr7rEkwfdQDuCir0wfBRuRTe2YWVsfqdigVXoSXbmVxyjnlgQFh6xxzpKmXIDegu5wVxoYglLEEV2h8Pq_7suE00rVAAOMGS",
@@ -14,3 +14,5 @@ keys:{
     p256dh:"BMmaeFYS5Qyww3sdm-u8yOmuA5QnSb600kj2559WO_7IUwQA8fdKW1k4HPO1MdFIAg0nrsOxN9VOlK7U46FXwoE",
 auth:"V4Uwnp_iXFVMB6TbB05q0Q"}
 };
+
+push.sendNotification(sub, 'test-message')
